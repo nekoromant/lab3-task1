e45ab03457dfbf81ff900837f5bd0f3dbf41b074
nekoromant <igorbog842@gmail.com>
2023-10-05 17:19:21 +0700

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿// Вариант 1
+// Дано число. Проверить является ли число положительным и двухзначным
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "rus");
+    int num;
+    cout << "Введите положительное двузначное число: ";
+    cin >> num;
+    if (num < 100 && num>9) cout << "Истина\n";
+    else cout << "Ложь\n"; 
+    system("pause");
+ }
